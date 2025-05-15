f5884e295356c2e4e04508bd9b0329ddc3a120cf
Borna Šepić <borna@vaangroup.com>
2025-05-15 16:35:27 +0200

    Add manifest icon

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -11,13 +11,8 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#000000',
     icons: [
       {
-        src: '/icon-192x192.png',
+        src: '/portic-logo-icon.jpg',
         sizes: '192x192',
-        type: 'image/png',
-      },
-      {
-        src: '/icon-512x512.png',
-        sizes: '512x512',
         type: 'image/png',
       },
     ],
